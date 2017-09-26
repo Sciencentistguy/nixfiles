16780f76cbd76057a00167d89a13d4810d09bfe0
Sciencentistguy <jamie.e.quigley@gmail.com>
2017-09-26 19:29:53 +0100

    removed suspend in win+L

--- i3/lock.sh
+++ i3/lock.sh
@@ -5,5 +5,5 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
-systemctl suspend
+#systemctl suspend
 rm /tmp/screen.png
