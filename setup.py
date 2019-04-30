0078e4c9b539bbdaeb7a93f609c3485da2a71b35
Sciencentistguy <jamie.e.quigley@gmail.com>
2019-04-30 16:19:24 +0100

    more setup.py

--- setup.py
+++ setup.py
@@ -7,27 +7,31 @@ pycodestyle = os.path.expanduser("~/.config/pycodestyle")
 i3 = os.path.expanduser("~/.config/i3")
 pacman = "/etc/pacman.conf"
 
-subprocess.run(["rm", "-rf", zshrc])
-subprocess.run(["ln", "-s", os.getcwd() + "/zshrc", zshrc])
-print("Installed zshrc")
+if ("n" not in input("Install .zshrc? (Y/n) ").lower()):
+    subprocess.run(["rm", "-rf", zshrc])
+    subprocess.run(["ln", "-s", os.getcwd() + "/zshrc", zshrc])
+    print("Installed zshrc")
 
-try:
-    os.makedirs("~/.config/nvim")
-except OSError:
-    pass
-subprocess.run(["rm", "-rf", initdotvim])
-subprocess.run(["ln", "-s", os.getcwd() + "/init.vim", initdotvim])
-print("Installed init.vim")
+if ("n" not in input("Install init.vim? (Y/n) ").lower()):
+    try:
+        os.makedirs("~/.config/nvim")
+    except OSError:
+        pass
+    subprocess.run(["rm", "-rf", initdotvim])
+    subprocess.run(["ln", "-s", os.getcwd() + "/init.vim", initdotvim])
+    print("Installed init.vim")
 
-subprocess.run(["rm", "-rf", pycodestyle])
-subprocess.run(["ln", "-s", os.getcwd() + "/pycodestyle", pycodestyle])
-print("Installed pycodestyle")
+if ("n" not in input("Install pycodestyle? (Y/n) ").lower()):
+    subprocess.run(["rm", "-rf", pycodestyle])
+    subprocess.run(["ln", "-s", os.getcwd() + "/pycodestyle", pycodestyle])
+    print("Installed pycodestyle")
 
-subprocess.run(["rm", "-rf", i3])
-subprocess.run(["ln", "-s", os.getcwd() + "/i3", i3])
-print("Installed i3 configs")
+if ("n" not in input("Install i3 configs? (Y/n) ").lower()):
+    subprocess.run(["rm", "-rf", i3])
+    subprocess.run(["ln", "-s", os.getcwd() + "/i3", i3])
+    print("Installed i3 configs")
 
 if ("n" not in input("Install pacman.conf? (Y/n) ").lower()):
     subprocess.run(["rm", "-rf", pacman])
-    subprocess.run(["ln", "-s", os.getcwd() + "/etc/pacman.conf", pacman])
+    subprocess.run(["sudo", "ln", "-s", os.getcwd() + "/etc/pacman.conf", pacman])
     print("Installed pacman config")
