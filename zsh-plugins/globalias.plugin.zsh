61363bc71e805301791bc833436bf449a64a83f3
Sciencentistguy <jamie.e.quigley@gmail.com>
2019-07-13 15:57:29 +0100

    fixed automatic alias expansion

--- zsh-plugins/globalias.plugin.zsh
+++ zsh-plugins/globalias.plugin.zsh
@@ -1,12 +1,10 @@
 globalias() {
-   if [[ $LBUFFER =~ ' [A-Z0-9]+$' ]]; then
-     zle _expand_alias
-   fi
-   zle self-insert
+    zle _expand_alias
+    zle self-insert
 }
 
 zle -N globalias
 
-bindkey " " globalias
-bindkey "^ " magic-space           # control-space to bypass completion
+bindkey " " magic-space
+bindkey "^ " globalias
 bindkey -M isearch " " magic-space # normal space during searches
