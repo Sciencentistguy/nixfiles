24b5fff81c2c94e3f6786111475d2fb711574b2a
Jamie Quigley <jamie@jamiequigley.com>
2020-07-22 21:56:28 +0100

    Updated ex() and update-music-library functions

--- functions.zsh
+++ functions.zsh
@@ -36,15 +36,11 @@ vmaf() {
 ex() {
     if [ -f $1 ]; then
         case $1 in
-        *.tar.bz2) tar xjf $1 ;;
-        *.tar.gz) tar xzf $1 ;;
-        *.tar.xz) tar xf $1 ;;
+        *.tar*) tar xvf $1 ;;
         *.bz2) bunzip2 $1 ;;
         *.rar) unrar x $1 ;;
         *.gz) gunzip $1 ;;
-        *.tar) tar xf $1 ;;
-        *.tbz2) tar xjf $1 ;;
-        *.tgz) tar xzf $1 ;;
+        *.t*z*) tar xvf $1 ;;
         *.zip) unzip $1 ;;
         *.Z) uncompress $1 ;;
         *.7z) 7z x $1 ;;
@@ -64,11 +60,13 @@ videodownload() {
 update-music-library() {
     if [ $(hostname) = chronos ]; then
         rsync -Pa --delete ~/Music/beets/ atlas:/nas/plex/Music
+        rsync -Pa ~/.config/beets atlas:/nas/plex/Music/beetsconfig
     fi
 }
 
 update-music-library-remote() {
     if [ $(hostname) = chronos ]; then
         rsync -Pa --delete ~/Music/beets/ home.jamiequigley.com:/nas/plex/Music
+        rsync -Pa ~/.config/beets home.jamiequigley.com:/nas/plex/Music/beetsconfig
     fi
 }
