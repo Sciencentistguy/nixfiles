91bebab77497b882c8cdd041b3a3df63864e68ec
Jamie Quigley <jamie@quigley.xyz>
2022-02-05 14:14:27 +0000

    SSH: Specify username for seedbox

--- nixpkgs/jamie.nix
+++ nixpkgs/jamie.nix
@@ -146,6 +146,7 @@ in
   programs.ssh.matchBlocks."gitlab.com".identityFile = "~/.ssh/github";
   programs.ssh.matchBlocks."*.gitlab.com".identityFile = "~/.ssh/github";
   programs.ssh.matchBlocks."seedbox".identityFile = "~/.ssh/seedbox";
+  programs.ssh.matchBlocks."seedbox".user = "sciencentistguy";
   programs.ssh.matchBlocks."aur.archlinux.org".identityFile = "~/.ssh/aur";
   programs.ssh.matchBlocks."*.york.ac.uk".user = "jehq500";
 
