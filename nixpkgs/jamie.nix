cac3b804d4ab91761273dac1fe6d8b2fe2ebf6d0
Jamie Quigley <jamie@quigley.xyz>
2022-02-05 14:44:26 +0000

    Nix: Comment each overridden package

--- nixpkgs/jamie.nix
+++ nixpkgs/jamie.nix
@@ -4,6 +4,7 @@ in
 {
   home.packages =
     let
+      # btop appears to be a little unstable on aarch64-darwin
       top =
         if isDarwin then
           pkgs.bpytop.overrideAttrs
@@ -13,6 +14,8 @@ in
               '';
             })
         else pkgs.btop;
+
+      # ffmpeg-full doesn't build on aarch64-darwin
       ffmpeg =
         if isDarwin then
           pkgs.ffmpeg
@@ -22,6 +25,8 @@ in
             fdkaacExtlib = true;
           };
 
+      # build neovim nightly rather than the most recent release in nixpkgs
+      # also link vi and vim to nvim
       neovim = neovim-nightly-pkgs.neovim-nightly.overrideAttrs (old: {
         postInstall = ''
           ln -s $out/bin/nvim $out/bin/vim
@@ -29,6 +34,7 @@ in
         '';
       });
 
+      # bundle beets with a custom plugin
       beets-with-file-info =
         pkgs.beets.overrideAttrs (oldAttrs: {
           propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or [ ]) ++ [
@@ -36,10 +42,11 @@ in
           ];
         });
 
+      # replace the default error messages in openssh with a suspicious character
       openssh-patched =
-        pkgs.openssh.overrideAttrs (old:
+        pkgs.openssh.overrideAttrs (oldAttrs:
           {
-            patches = old.patches or [ ] ++ [
+            patches = oldAttrs.patches or [ ] ++ [
               (pkgs.fetchpatch {
                 url = "https://gist.githubusercontent.com/Sciencentistguy/18f94bde5ce06b63d760a736322b1aa0/raw/be75bcf34fe98fd49a68d2fccf366c5e51c06915/sussh.patch";
                 sha256 = "sha256-PKrlujoTkvTDCl02AGR5vmlZVeSoLYZqQGERc1sI+hM";
@@ -49,8 +56,9 @@ in
             checkTarget = [ ];
           });
 
-      exa-patched = pkgs.exa.overrideAttrs (old: {
-        patches = old.patches or [ ] ++ [
+      # use a custom colourscheme for exa
+      exa-patched = pkgs.exa.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches or [ ] ++ [
           (pkgs.fetchpatch {
             url = "https://gist.githubusercontent.com/Sciencentistguy/8d48b464d1e5846b55e61854887cc5af/raw/e43d9c4344954b1ae9be9dd0faf1d2e44276ea0f/exa-onedark.patch";
             sha256 = "sha256-JTnumRCJuX6WHcSKBXYCvabDMoKLnBVMqju2TbdEj2E";
@@ -105,7 +113,6 @@ in
       pkgs.shfmt
       pkgs.python3Packages.autopep8
       pkgs.stylua
-
 
       # Broken on aarch64-darwin
       beets-with-file-info # Music orginaisation software with a custom plugin
