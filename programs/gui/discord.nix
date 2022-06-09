60919c632db8a1e47dc5ddf26050c27454cdef2b
Jamie Quigley <jamie@quigley.xyz>
2022-06-09 13:21:56 +0100

    Prevent discord from breaking older versions
    Nixpkgs usually takes a while to propagate the new version through to
    unstable. See https://github.com/NixOS/nixpkgs/pull/176937

--- programs/gui/discord.nix
+++ programs/gui/discord.nix
@@ -1,3 +1,18 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [discord];
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  home.packages = [pkgs.discord];
+
+
+  home.file.".config/discord/settings.json" = {
+    text = builtins.toJSON {
+      "BACKGROUND_COLOR" = "#202225";
+      "IS_MAXIMIZED" = true;
+      "IS_MINIMIZED" = false;
+      # Discord likes to break old versions. Don't do that
+      "SKIP_HOST_UPDATE" = true;
+    };
+  };
 }
