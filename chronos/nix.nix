91110586f057ccb239b6be6c977a977229d6c8b9
Jamie Quigley <jamie@quigley.xyz>
2022-06-10 21:03:56 +0100

    Add fenix to NIX_PATH

--- chronos/nix.nix
+++ chronos/nix.nix
@@ -20,10 +20,16 @@
   # the version of nixpkgs used to build the system
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   environment.etc."nix/path/nixpkgs".source = inputs.nixpkgs;
+
   # the version of home-manager used to build the system
   nix.registry.home-manager.flake = inputs.home-manager;
   environment.etc."nix/path/home-manager".source = inputs.home-manager;
+
   # the version of this flake used to build the system
   nix.registry.activeconfig.flake = inputs.self;
   environment.etc."nix/path/activeconfig".source = inputs.self;
+
+  # Fenix
+  nix.registry.fenix.flake = inputs.fenix;
+  environment.etc."nix/path/fenix".source = inputs.fenix;
 }
