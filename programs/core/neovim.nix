31edf334ec13a9a9f10aa345542a0a0838323f41
Jamie Quigley <jamie@quigley.xyz>
2022-06-17 15:52:54 +0100

    Use wrapped neovim to avoid polluting PATH

--- programs/core/neovim.nix
+++ programs/core/neovim.nix
@@ -6,50 +6,58 @@
 }: let
   # build neovim nightly rather than the most recent release in nixpkgs
   # also link vi and vim to nvim
-  neovim = pkgs.neovim-nightly.overrideAttrs (old: {
+  neovim' = pkgs.neovim-nightly.overrideAttrs (old: {
     postInstall = ''
       ln -s $out/bin/nvim $out/bin/vim
       ln -s $out/bin/nvim $out/bin/vi
     '';
   });
+
+  # Nvim needs things from path; don't install them globally just because of that.
+  neovim = pkgs.symlinkJoin {
+    name = "neovim";
+    paths = [neovim'];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = let
+      python = pkgs.python3.withPackages (pp: with pp; [pynvim black]);
+    in ''
+      wrapProgram $out/bin/nvim\
+        --prefix PATH : ${lib.makeBinPath (with pkgs;
+        [
+          curl
+          fd
+          fzf
+          git
+          nodejs
+          perl
+          python
+          ripgrep
+          stdenv.cc
+          tree-sitter
+          zathura
+        ]
+        ++ lib.optionals (!isDarwin) [
+          pkgs.xclip
+        ])}
+    '';
+  };
 in {
-  home.packages =
-    [
-      neovim
+  home.packages = [
+    neovim
 
-      # Runtimes
-      (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [pynvim]))
-      pkgs.nodejs
+    # Linters
+    pkgs.shellcheck
 
-      # Linters
-      pkgs.shellcheck
+    # Formatters
+    pkgs.shfmt
+    pkgs.stylua
+    pkgs.alejandra
+    pkgs.nodePackages.prettier
 
-      # Formatters
-      pkgs.shfmt
-      pkgs.stylua
-      pkgs.alejandra
-      pkgs.nodePackages.prettier
-
-      # Language servers
-      # Pin rust-analyzer. See https://github.com/rust-lang/rust-analyzer/issues/12482
-      # pkgs.rust-analyzer-nightly
-      (
-        pkgs.fenix.toolchainOf {
-          channel = "nightly";
-          date = "2022-05-30";
-          sha256 = "sha256-N9V9NJ3BDUC81tIbYxAXQemrhR1l9jLd5t9L70NfcZo=";
-        }
-      )
-      .rust-analyzer-preview
-      pkgs.sumneko-lua-language-server
-      pkgs.nodePackages.pyright
-      pkgs.rnix-lsp
-
-      # Utilities
-      pkgs.fzf
-    ]
-    ++ lib.optionals (!isDarwin) [
-      pkgs.xclip
-      pkgs.python3Packages.black
-    ];
+    # Language servers
+    pkgs.rust-analyzer-nightly
+    pkgs.sumneko-lua-language-server
+    pkgs.nodePackages.pyright
+    pkgs.rnix-lsp
+  ];
 }
