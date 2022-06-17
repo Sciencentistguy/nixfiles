d78a2b2806185f7530bbb0d3a18dafb2f3fa81ea
Jamie Quigley <jamie@quigley.xyz>
2022-06-17 15:40:30 +0100

    Use packaged zsh-vi-mode plugin and oh-my-zsh plugins

--- programs/core/zsh.nix
+++ programs/core/zsh.nix
@@ -10,15 +10,8 @@
   home.file.".zsh/plugins/zsh-autosuggestions".source = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
   home.file.".zsh/plugins/zsh-nix-shell".source = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
   home.file.".zsh/plugins/zsh-syntax-highlighting".source = "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting";
-  home.file.".zsh/plugins/zsh-vim-mode".source = let
-    zsh-vim-mode = pkgs.fetchFromGitHub {
-      owner = "softmoth";
-      repo = "zsh-vim-mode";
-      rev = "1f9953b7d6f2f0a8d2cb8e8977baa48278a31eab";
-      sha256 = "sha256-a+6EWMRY1c1HQpNtJf5InCzU7/RphZjimLdXIXbO6cQ=";
-    };
-  in "${zsh-vim-mode}";
+  home.file.".zsh/plugins/zsh-vi-mode".source = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode/";
 
-  home.file.".zsh/plugins/git.plugin.zsh".source = "${inputs.dotfiles}/zsh-plugins/git.plugin.zsh";
-  home.file.".zsh/plugins/globalias.plugin.zsh".source = "${inputs.dotfiles}/zsh-plugins/globalias.plugin.zsh";
+  home.file.".zsh/plugins/git.plugin.zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/git/git.plugin.zsh";
+  home.file.".zsh/plugins/globalias.plugin.zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/globalias/globalias.plugin.zsh";
 }
