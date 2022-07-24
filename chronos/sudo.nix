214b274bea3c75ff15eaf3f9f4f13a27376bf59c
Jamie Quigley <jamie@quigley.xyz>
2022-07-24 11:49:02 +0100

    chronos: sudo: remove sus patch

--- chronos/sudo.nix
+++ chronos/sudo.nix
@@ -1,6 +1,3 @@
 {pkgs, ...}: {
   security.sudo.wheelNeedsPassword = false;
-  security.sudo.package = pkgs.sudo.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [./susdo.patch];
-  });
 }
