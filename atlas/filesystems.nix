b8025f348cdd028bc30008061f2ced3b55732dd2
Jamie Quigley <jamie@quigley.xyz>
2022-07-24 12:28:36 +0100

    atlas: add ro bind mounts for plex content

--- atlas/filesystems.nix
+++ atlas/filesystems.nix
@@ -5,4 +5,29 @@
       fsType = "btrfs";
     };
   };
+
+  #Virtual fielsystems
+  fileSystems = {
+    "/music" = {
+      device = "/nas/plex/Music";
+      options = [
+        "bind"
+        "ro"
+      ];
+    };
+    "/tv" = {
+      device = "/nas/plex/TV";
+      options = [
+        "bind"
+        "ro"
+      ];
+    };
+    "/movies" = {
+      device = "/nas/plex/Films";
+      options = [
+        "bind"
+        "ro"
+      ];
+    };
+  };
 }
