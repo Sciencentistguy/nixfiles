343985660403c6282209259dafc577f7d3f77c20
Jamie Quigley <jamie@quigley.xyz>
2022-07-31 15:30:32 +0100

    Use nix-index for command-not-found
    The nixos one depends on channels 🙃.

--- home/core/zsh.nix
+++ home/core/zsh.nix
@@ -19,26 +19,5 @@
   home.file.".zsh/plugins/git.plugin.zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/git/git.plugin.zsh";
   home.file.".zsh/plugins/globalias.plugin.zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/globalias/globalias.plugin.zsh";
 
-  # Another nixos-module import from home-manager. I'd quite like it if this wasn't as hideous as it is.
-  home.file.".zsh/command-not-found.zsh".text = let
-    raw-nixos-module = import "${inputs.nixpkgs}/nixos/modules/programs/command-not-found/command-not-found.nix";
-    dbPath =
-      (raw-nixos-module {
-        inherit pkgs lib;
-        config = {};
-      })
-      .options
-      .programs
-      .command-not-found
-      .dbPath
-      .default;
-    nixos-module = raw-nixos-module {
-      inherit pkgs lib;
-      config = {
-        programs.command-not-found.enable = true;
-        programs.command-not-found.dbPath = dbPath;
-      };
-    };
-  in
-    nixos-module.config.content.programs.zsh.interactiveShellInit;
+  home.file.".zsh/command-not-found.zsh".source = "${pkgs.nix-index-unwrapped}/etc/profile.d/command-not-found.sh";
 }
