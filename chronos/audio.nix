d2109ac741f6742bf209aa305834b8362e9f7699
Jamie Quigley <jamie@quigley.xyz>
2022-08-08 11:49:30 +0100

    Revert "chronos: let pipewire choose its own settings"
    This reverts commit 3f789760e064915d025235d2cda2748ff1c330d2.

--- chronos/audio.nix
+++ chronos/audio.nix
@@ -2,5 +2,18 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire.enable = true;
+  services.pipewire = {
+    enable = true;
+    wireplumber.enable = true;
+    media-session.enable = false;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    config.pipewire = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [44100 48000 88200 96000 176400 192000 352800];
+      };
+    };
+  };
 }
