b5e0d92be5b187b17e6e765aa3dcaac8d85b9823
Jamie Quigley <jamie@quigley.xyz>
2022-08-24 16:08:58 +0100

    chronos: some network fiddling

--- chronos/networking.nix
+++ chronos/networking.nix
@@ -6,10 +6,11 @@
   networking.hostName = systemName; # Define your hostname.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
-  networking.firewall.enable = false;
   services.openssh.enable = true;
   services.mullvad-vpn.enable = true;
   services.tailscale.enable = true;
+
+  services.resolved.enable = true;
 
   environment.systemPackages = with pkgs; [mullvad-vpn tailscale];
 
@@ -17,7 +18,26 @@
   systemd.services.tailscaled.serviceConfig.LimitNOFILE = "infinity";
 
   networking.nameservers = ["1.1.1.1" "8.8.8.8"];
-  networking.hosts = {
-    "192.168.1.51" = ["atlas"];
-  };
+
+  networking.hosts."192.168.1.51" = ["atlas"];
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedUDPPorts = [41641];
+
+  networking.firewall.extraCommands = ''
+    ${pkgs.nftables}/bin/nft -f - <<EOF
+    table inet mullvad-ts {
+      chain exclude-outgoing {
+        type route hook output priority 0; policy accept;
+        ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+      }
+
+      chain allow-incoming {
+        type filter hook input priority -100; policy accept;
+        iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+      }
+    }
+    EOF
+  '';
 }
