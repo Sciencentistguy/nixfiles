e81b29f79db1aa948aa633ee1a6c732072ae2e7f
Jamie Quigley <jamie@quigley.xyz>
2022-08-27 17:18:02 +0100

    atlas: plex: set PIDfile in systemd service

--- atlas/services/plex.nix
+++ atlas/services/plex.nix
@@ -4,4 +4,6 @@
     dataDir = "/nas/plex/library_data";
     openFirewall = true;
   };
+
+  systemd.services.plex.serviceConfig.PIDFile = "/nas/plex/library_data/Plex Media Server/plexmediaserver.pid";
 }
