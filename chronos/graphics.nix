b6f88aa3064804d6dd7e6879ed8723582d001e33
Jamie Quigley <jamie@quigley.xyz>
2022-10-07 12:35:51 +0100

    chronos: enable openGL vaapi and 32-bit

--- chronos/graphics.nix
+++ chronos/graphics.nix
@@ -3,5 +3,16 @@
   services.xserver.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
 
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      nvidia-vaapi-driver
+      intel-media-driver
+      vaapiVdpau
+      libvdpau
+    ];
+  };
+
   environment.systemPackages = with pkgs; [nvtop];
 }
