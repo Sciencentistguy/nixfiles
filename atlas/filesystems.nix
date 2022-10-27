b55a2310d285106dc67bc8243002e5da91ceec7e
Jamie Quigley <jamie@quigley.xyz>
2022-10-27 22:58:04 +0100

    atlas: remove /nas filesystem

--- atlas/filesystems.nix
+++ atlas/filesystems.nix
@@ -1,18 +1,11 @@
 {pkgs, ...}: {
   boot.supportedFilesystems = ["zfs"];
-  boot.zfs.extraPools = ["storage-pool"];
+  boot.zfs.extraPools = ["scratch" "storage-pool"];
 
   # limit ZFS arc size to 16 GiB
   boot.kernelParams = let
     bytes = 16 * (1024 * 1024 * 1024);
   in ["zfs.zfs_arc_max=${toString bytes}"];
-
-  fileSystems = {
-    "/nas" = {
-      device = "/dev/disk/by-uuid/50830e2e-c132-4171-ab77-224e7f505c06";
-      fsType = "btrfs";
-    };
-  };
 
   boot.tmpOnTmpfs = true;
 }
