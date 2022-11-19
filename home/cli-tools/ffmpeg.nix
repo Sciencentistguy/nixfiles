ddb50eebb8fea5e47582c64561f96a038c821c54
Jamie Quigley <jamie@quigley.xyz>
2022-11-19 15:11:04 +0000

    home/cli-tools/ffmpeg: use nnedi_weights from videoconverter

--- home/cli-tools/ffmpeg.nix
+++ home/cli-tools/ffmpeg.nix
@@ -18,8 +18,5 @@ in {
   ];
 
   # ffmpeg nnedi filter needs weights downloaded separately
-  home.file.".ffmpeg/nnedi3_weights".source = builtins.fetchurl {
-    url = "https://github.com/dubhater/vapoursynth-nnedi3/raw/cc6f6065e09c9241553cb51f10002a7314d66bfa/src/nnedi3_weights.bin";
-    sha256 = "0hhx4n19qaj3g68f5kqjk23cj063g4y2zidivq9pdfrm0i1q5wr7";
-  };
+  home.file.".ffmpeg/nnedi3_weights".source = flakePkgs.nnedi_weights;
 }
