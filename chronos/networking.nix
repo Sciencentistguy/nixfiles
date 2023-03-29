7e6572d938a1e07d6561e0b1fdde9dacb9530fb1
Jamie Quigley <jamie@quigley.xyz>
2023-03-29 21:28:27 +0100

    chronos/networking: allow all tailscale traffic through firewall

--- chronos/networking.nix
+++ chronos/networking.nix
@@ -24,6 +24,7 @@
 
   networking.firewall.enable = true;
   networking.firewall.allowedUDPPorts = [41641];
+  networking.firewall.trustedInterfaces = [ "tailscale0" ];
 
   networking.firewall.extraCommands = ''
     ${pkgs.nftables}/bin/nft -f - <<EOF
