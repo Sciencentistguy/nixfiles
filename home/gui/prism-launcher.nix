6b30d10fbfff85d41eabdb2e345d67e2e3b4b806
Jamie Quigley <jamie@quigley.xyz>
2023-04-01 17:49:41 +0100

    home/gui/prism-launcher: add more java versions

--- home/gui/prism-launcher.nix
+++ home/gui/prism-launcher.nix
@@ -3,5 +3,9 @@
   flakePkgs,
   ...
 }: {
-  home.packages = with flakePkgs; [prismlauncher];
+  home.packages = let
+    prismlauncher = flakePkgs.prismlauncher.override {
+      jdks = with pkgs; [jdk8 jdk17 jdk];
+    };
+  in [prismlauncher];
 }
