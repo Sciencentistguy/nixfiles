a62fdc6820960bb6035239bc9e2f6bfadeb23fe7
Jamie Quigley <jamie@quigley.xyz>
2023-04-10 00:01:31 +0100

    home/gui/spotify: fix external links

--- home/gui/spotify.nix
+++ home/gui/spotify.nix
@@ -1,3 +1,9 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [spotify];
+  home.packages = let
+    # This is needed to fix external links being broken in spotify.
+    # See: https://github.com/NixOS/nixpkgs/issues/47885
+    spotify = pkgs.spotify.override {
+      nss = pkgs.nss_latest;
+    };
+  in [spotify];
 }
