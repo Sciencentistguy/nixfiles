ba32f1f8b7889e09719e0eafd66a23c4e9c77cda
Jamie Quigley <jamie@quigley.xyz>
2023-04-26 10:06:04 +0100

    cli-tools: exclude whipper from discordia

--- home/cli-tools/default.nix
+++ home/cli-tools/default.nix
@@ -1,32 +1,35 @@
-{...}: {
-  imports = [
-    ./archive-utils.nix
-    ./bat.nix
-    ./btop.nix
-    ./comma.nix
-    ./exa.nix
-    ./fd.nix
-    ./ffmpeg.nix
-    ./file.nix
-    ./gping.nix
-    ./hyfetch
-    ./jq.nix
-    ./killall.nix
-    ./mpv.nix
-    ./ncdu.nix
-    ./procs.nix
-    ./rink.nix
-    ./ripgrep.nix
-    ./rsync.nix
-    ./rust-nix-shell.nix
-    ./sad.nix
-    ./scripts.nix
-    ./shark-radar.nix
-    ./speedtest.nix
-    ./tmux.nix
-    ./watch.nix
-    ./wget.nix
-    ./whipper
-    ./yt-dlp.nix
-  ];
+{lib, systemName, ...}: {
+  imports =
+    [
+      ./archive-utils.nix
+      ./bat.nix
+      ./btop.nix
+      ./comma.nix
+      ./exa.nix
+      ./fd.nix
+      ./ffmpeg.nix
+      ./file.nix
+      ./gping.nix
+      ./hyfetch
+      ./jq.nix
+      ./killall.nix
+      ./mpv.nix
+      ./ncdu.nix
+      ./procs.nix
+      ./rink.nix
+      ./ripgrep.nix
+      ./rsync.nix
+      ./rust-nix-shell.nix
+      ./sad.nix
+      ./scripts.nix
+      ./shark-radar.nix
+      ./speedtest.nix
+      ./tmux.nix
+      ./watch.nix
+      ./wget.nix
+      ./yt-dlp.nix
+    ]
+    ++ lib.optionals (systemName != "discordia") [
+      ./whipper
+    ];
 }
