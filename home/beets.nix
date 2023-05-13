b8106cfb86d4d453c08da711e9e06e56e25510a8
Jamie Quigley <jamie@quigley.xyz>
2023-05-13 10:13:20 +0100

    home/beets: unset fetchart.minwidth

--- home/beets.nix
+++ home/beets.nix
@@ -51,9 +51,9 @@ in {
       "missing"
       "scrub"
     ];
-    # Ideally this would be higher, but if the only art that exists is <1000, I still want to
-    # fetch it.
-    fetchart.minwidth = 500;
+    # Ideally this would be set, but if the only art that exists is <1000, I still want to fetch
+    # it.
+    # fetchart.minwidth = 1000;
     fetchart.sources = [
       "filesystem"
       {coverart = "release";}
