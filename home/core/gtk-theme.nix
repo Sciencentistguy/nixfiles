2e364ae5dda12114223e79f1c14227beda541d2a
Jamie Quigley <jamie@quigley.xyz>
2023-05-27 00:12:45 +0100

    home/core/gtk: specify theme better

--- home/core/gtk-theme.nix
+++ home/core/gtk-theme.nix
@@ -3,9 +3,17 @@
   flakePkgs,
   ...
 }: {
-  gtk = {
+  gtk = let
+    dark-mode-selection = {
+      gtk-application-prefer-dark-theme = 0;
+    };
+  in {
     enable = true;
-    cursorTheme.package = flakePkgs.apple-cursor-theme;
-    cursorTheme.name = "MacOSMonterey";
+    cursorTheme = {
+      name = "MacOSMonterey";
+      package = flakePkgs.apple-cursor-theme;
+    };
+    gtk3.extraConfig = dark-mode-selection;
+    gtk4.extraConfig = dark-mode-selection;
   };
 }
