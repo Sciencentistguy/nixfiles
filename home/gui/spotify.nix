3b030cb787cb79d47d9e30c6478b7c537e7a6184
Jamie Quigley <jamie@quigley.xyz>
2023-05-27 00:13:32 +0100

    home/gui/spotify: remove override
    its no longer needed

--- home/gui/spotify.nix
+++ home/gui/spotify.nix
@@ -1,8 +1,3 @@
 {pkgs, ...}: {
-  home.packages = let
-    # Horrible hack - see https://github.com/NixOS/nixpkgs/issues/227449
-    spotify = pkgs.spotify.override {
-      callPackage = p: attrs: pkgs.callPackage p (attrs // {nss = pkgs.nss_latest;});
-    };
-  in [spotify];
+  home.packages = with pkgs; [spotify];
 }
