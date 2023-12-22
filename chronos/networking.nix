4dbec9057c176a77588a94da1976ce81ea617cb7
Jamie Quigley <jamie@quigley.xyz>
2023-12-22 22:18:57 +0000

    chronos: (TEMP): fiddle with tailscale settings

--- chronos/networking.nix
+++ chronos/networking.nix
@@ -12,38 +12,46 @@
 
   services.resolved.enable = true;
 
-  environment.systemPackages = with pkgs; [mullvad-vpn tailscale];
+  environment.systemPackages = with pkgs; [
+    mullvad-vpn
+    tailscale
+  ];
 
   # Tailscale ran out of fds ??
   systemd.services.tailscaled.serviceConfig.LimitNOFILE = "infinity";
 
-  networking.nameservers = ["100.100.100.100" "1.1.1.1" "8.8.8.8"];
-  networking.search = ["axolotl-shark.ts.net"];
+  # networking.nameservers = [
+  # "100.100.100.100"
+  # "1.1.1.1"
+  # "8.8.8.8"
+  # ];
+  # networking.search = ["axolotl-shark.ts.net"];
 
   networking.hosts."192.168.1.3" = ["atlas"];
 
-  networking.firewall.enable = true;
+  # networking.firewall.enable = true;
+  networking.firewall.enable = false;
   networking.firewall.allowedUDPPorts = [41641];
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
+  networking.firewall.trustedInterfaces = ["tailscale0"];
 
   networking.firewall.extraCommands = ''
     ${pkgs.nftables}/bin/nft -f - <<EOF
     table inet mullvad-ts {
-      chain exclude-outgoing {
-        type route hook output priority 0; policy accept;
-        ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-        ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-      }
-      chain excludeDns {
-        type filter hook output priority -10; policy accept;
-        ip daddr 100.00.100.100 udp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-        ip daddr 100.00.100.100 tcp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-      }
+        chain exclude-outgoing {
+            type route hook output priority 0; policy accept;
+            ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        }
+        chain excludeDns {
+            type filter hook output priority -10; policy accept;
+            ip daddr 100.00.100.100 udp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+            ip daddr 100.00.100.100 tcp dport 53 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        }
 
-      chain allow-incoming {
-        type filter hook input priority -100; policy accept;
-        iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-      }
+        chain allow-incoming {
+            type filter hook input priority -100; policy accept;
+            iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        }
     }
     EOF
   '';
