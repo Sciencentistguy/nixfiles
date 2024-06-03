cfcc8afcca7d6f684a6a656eececb038605b34ea
Jamie Quigley <jamie@quigley.xyz>
2024-06-03 15:44:05 +0100

    chronos: force gdm to use xorg

--- chronos/gnome/default.nix
+++ chronos/gnome/default.nix
@@ -5,6 +5,7 @@
 }: {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.desktopManager.gnome = {
     extraGSettingsOverridePackages = with pkgs; [gnome.gnome-settings-daemon];
