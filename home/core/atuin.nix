198e91bb9381d349fcf070d21c32467ce8a655df
Jamie Quigley <jamie@quigley.xyz>
2024-08-02 10:02:02 +0100

    atuin: specify config

--- home/core/atuin.nix
+++ home/core/atuin.nix
@@ -1,3 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [atuin];
+  xdg.configFile."atuin/config.toml".source = (pkgs.formats.toml {}).generate "atuin-config" {
+    dialect = "uk";
+    auto_sync = true;
+    sync_address = "https://api.atuin.sh";
+    search_mode = "fulltext";
+  };
 }
