762296d79559b55655af353311ad14b54b73c3dd
Jamie Quigley <jamie@quigley.xyz>
2024-08-02 11:13:57 +0100

    whipper: remove

--- home/cli-tools/default.nix
+++ home/cli-tools/default.nix
@@ -1,37 +1,37 @@
-{lib, systemName, ...}: {
-  imports =
-    [
-      ./archive-utils.nix
-      ./bat.nix
-      ./btop.nix
-      ./comma.nix
-      ./exiftool.nix
-      ./eza.nix
-      ./fd.nix
-      ./ffmpeg.nix
-      ./file.nix
-      ./gping.nix
-      ./hyfetch
-      ./jq.nix
-      ./killall.nix
-      ./mpv.nix
-      ./ncdu.nix
-      ./procs.nix
-      ./qobuz-identifier.nix
-      ./rink.nix
-      ./ripgrep.nix
-      ./rsync.nix
-      ./rust-nix-shell.nix
-      ./sad.nix
-      ./scripts.nix
-      ./shark-radar.nix
-      ./speedtest.nix
-      ./tmux.nix
-      ./watch.nix
-      ./wget.nix
-      ./yt-dlp.nix
-    ]
-    ++ lib.optionals (systemName != "discordia") [
-      ./whipper
-    ];
+{
+  lib,
+  systemName,
+  ...
+}: {
+  imports = [
+    ./archive-utils.nix
+    ./bat.nix
+    ./btop.nix
+    ./comma.nix
+    ./exiftool.nix
+    ./eza.nix
+    ./fd.nix
+    ./ffmpeg.nix
+    ./file.nix
+    ./gping.nix
+    ./hyfetch
+    ./jq.nix
+    ./killall.nix
+    ./mpv.nix
+    ./ncdu.nix
+    ./procs.nix
+    ./qobuz-identifier.nix
+    ./rink.nix
+    ./ripgrep.nix
+    ./rsync.nix
+    ./rust-nix-shell.nix
+    ./sad.nix
+    ./scripts.nix
+    ./shark-radar.nix
+    ./speedtest.nix
+    ./tmux.nix
+    ./watch.nix
+    ./wget.nix
+    ./yt-dlp.nix
+  ];
 }
