912d20b91e20f434b874463248c3088b61626870
Jamie <jamie@quigley.xyz>
2025-04-01 10:48:11 +0000

    atuin: use home-manager module

--- home/core/atuin.nix
+++ home/core/atuin.nix
@@ -1,9 +1,13 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [atuin];
-  xdg.configFile."atuin/config.toml".source = (pkgs.formats.toml {}).generate "atuin-config" {
-    dialect = "uk";
-    auto_sync = true;
-    sync_address = "https://api.atuin.sh";
-    search_mode = "fulltext";
+{...}: {
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = false;
+    settings = {
+      dialect = "uk";
+      auto_sync = true;
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fulltext";
+    };
+    daemon.enable = true;
   };
 }
