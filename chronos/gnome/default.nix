8139ece451fd7df911571d6fa0b24570cced7172
Jamie Quigley <jamie@quigley.xyz>
2025-06-08 15:50:26 +0100

    chronos: disable wayland

--- chronos/gnome/default.nix
+++ chronos/gnome/default.nix
@@ -5,7 +5,7 @@
 }: {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.desktopManager.gnome = {
     extraGSettingsOverridePackages = with pkgs; [gnome-settings-daemon];
