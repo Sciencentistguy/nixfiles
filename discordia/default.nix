5dbb70e38771cf5d72e3f50c626c85b30c1863af
Jamie Quigley <jamie@quigley.xyz>
2025-09-06 12:49:29 +0100

    discordia: follow nixpkgs changes

--- discordia/default.nix
+++ discordia/default.nix
@@ -41,6 +41,7 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+  system.primaryUser = "jamie";
 
   ids.gids.nixbld = 350;
 }
