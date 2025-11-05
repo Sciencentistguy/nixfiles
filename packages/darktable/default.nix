a2a84648b76a33d2eeafa98c1a4dd32e87c14306
Jamie Quigley <jamie@quigley.xyz>
2025-11-05 10:57:43 +0000

    darktable: fix build on macos

--- packages/darktable/default.nix
+++ packages/darktable/default.nix
@@ -1,11 +1,17 @@
 {
+lib,
+stdenv,
   darktable,
   darktable-src,
 }:
-darktable.overrideAttrs (_: {
+darktable.overrideAttrs (old: {
   src = darktable-src;
   # dontCheck = true;
   installCheckPhase = ''
     echo skipped...
   '';
+
+    cmakeFlags = old.cmakeFlags ++ (lib.optionals (stdenv.isDarwin) [
+        "-DCMAKE_OSX_DEPLOYMENT_TARGET=26"
+    ]);
 })
