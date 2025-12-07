905ec0348a652b59a3d2b1752a31eaeb38e8fae6
Jamie Quigley <jamie@quigley.xyz>
2025-12-07 16:04:18 +0000

    chronos: remove xorg

--- chronos/graphics.nix
+++ chronos/graphics.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.graphics = {
