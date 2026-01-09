c8dcae8ba77bf5fa2ea2eedf564f5fb9f36e4bc3
Jamie Quigley <jamie@quigley.xyz>
2026-01-09 09:31:35 +0000

    spotify: disable wayland

--- home/gui/spotify.nix
+++ home/gui/spotify.nix
@@ -1,3 +1,11 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.spotify];
+  home.packages = let
+    spotify = pkgs.spotify.overrideAttrs (old: {
+      # Under wayland spotify gains an ugly blue window border. Explicitly disable wayland in the
+      # shell wrapper
+      postFixup = ''
+        sed -i '$i unset WAYLAND_DISPLAY' $out/share/spotify/spotify
+      '';
+    });
+  in [spotify];
 }
