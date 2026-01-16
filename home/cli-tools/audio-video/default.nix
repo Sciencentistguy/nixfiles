b03c2eb320cd086624c1469ad125f8aea541f17a
Jamie Quigley <jamie@quigley.xyz>
2026-01-16 16:25:27 +0000

    mpv: enable mpris support

--- home/cli-tools/audio-video/default.nix
+++ home/cli-tools/audio-video/default.nix
@@ -6,7 +6,10 @@
   ...
 }: {
   imports = [./ffmpeg];
-  home.packages =
+  home.packages = let
+    mpv = pkgs.mpv.override {scripts = [pkgs.mpvScripts.mpris];};
+    plex-mpv-shim = pkgs.plex-mpv-shim.override {inherit mpv;};
+  in
     [
       flakePkgs.dr14_tmeter
       flakePkgs.qobuz-identifier
@@ -14,7 +17,7 @@
       pkgs.yt-dlp
     ]
     ++ lib.optionals (systemName == "chronos") [
-      pkgs.mpv
-      (pkgs.callPackage ./plex-mpv-shim.nix {})
+      mpv
+      (pkgs.callPackage ./plex-mpv-shim.nix {inherit plex-mpv-shim;})
     ];
 }
