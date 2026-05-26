6ecca26dbb5b5decc5a94c2b268b97857968b29e
Jamie Quigley <jamie@quigley.xyz>
2026-05-26 14:02:33 +0100

    chronos: specify firefox config dir

--- home/gui/web-browser.nix
+++ home/gui/web-browser.nix
@@ -3,6 +3,9 @@
   flakePkgs,
   ...
 }: {
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    configPath = ".mozilla/firefox"; # explicitly use old default behaviour
+  };
   home.packages = [pkgs.google-chrome flakePkgs.helium];
 }
