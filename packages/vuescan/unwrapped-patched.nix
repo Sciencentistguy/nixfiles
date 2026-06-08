0f8734cfd8c3f1e7f17b4ad3e2547014127126eb
Jamie Quigley <jamie@quigley.xyz>
2026-06-08 23:01:23 +0100

    vuescan: provide desktop file

---
+++ packages/vuescan/unwrapped-patched.nix
@@ -0,0 +1,55 @@
+{
+  autoPatchelfHook,
+  stdenvNoCC,
+  udev,
+  alsa-lib,
+  gtk2,
+  gtk3,
+  gtk4,
+  gdk-pixbuf,
+  cairo,
+  pango,
+  fontconfig,
+  glib,
+  freetype,
+  libuuid,
+  zlib,
+  libX11,
+  libXcursor,
+  libXrandr,
+  libSM,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "vuescan";
+  version = "9.8.26";
+  src = fetchTarball {
+    url = "https://www.hamrick.com/files/vuex6498.tgz";
+    sha256 = "12gjjkw3cnn9bfwmn48r3cw73wpbcwpzwwj21s1rdvgsdh0w99dq";
+  };
+  dontBuild = true;
+  nativeBuildInputs = [autoPatchelfHook];
+  buildInputs = [
+    udev
+    alsa-lib
+    gtk2
+    gtk3
+    gtk4
+    gdk-pixbuf
+    cairo
+    pango
+    fontconfig
+    glib
+    freetype
+    libuuid
+    zlib
+    libX11
+    libXcursor
+    libXrandr
+    libSM
+  ];
+  installPhase = ''
+    install -Dm644 vuescan.svg $out/share/icons/hicolor/scalable.apps/vuescan.svg
+    install -Dm644 vuescan.rul $out/lib/udev/rules.d/60-vuescan.rules
+    install -Dm755 vuescan $out/bin/vuescan
+  '';
+}
--- packages/vuescan/wrapper.nix
+++ packages/vuescan/wrapper.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  stdenvNoCC,
+  symlinkJoin,
   buildFHSEnv,
   makeDesktopItem,
   vuescan-unwrapped,
@@ -21,28 +21,62 @@
   libXcursor,
   libXrandr,
   libSM,
-}:
-buildFHSEnv {
-  name = "vuescan";
-  targetPkgs = pkgs: [
-    vuescan-unwrapped
-    udev
-    alsa-lib
-    gtk2
-    gtk3
-    gtk4
-    gdk-pixbuf
-    cairo
-    pango
-    fontconfig
-    glib
-    freetype
-    libuuid
-    zlib
-    libX11
-    libXcursor
-    libXrandr
-    libSM
-  ];
-  runScript = "vuescan";
-}
+}: let
+  fhsEnv = buildFHSEnv {
+    name = "vuescan";
+    targetPkgs = pkgs: [
+      vuescan-unwrapped
+      udev
+      alsa-lib
+      gtk2
+      gtk3
+      gtk4
+      gdk-pixbuf
+      cairo
+      pango
+      fontconfig
+      glib
+      freetype
+      libuuid
+      zlib
+      libX11
+      libXcursor
+      libXrandr
+      libSM
+    ];
+    runScript = "vuescan";
+  };
+
+  vuescan-desktop = makeDesktopItem {
+    name = "vuescan";
+    exec = "vuescan";
+    icon = "vuescan";
+    desktopName = "VueScan";
+    genericName = "Scanning Software";
+    comment = "Scan documents and photos";
+    categories = ["Graphics" "Scanning"];
+    terminal = false;
+  };
+in
+  symlinkJoin {
+    pname = "vuescan";
+    version = "1.0";
+
+    phases = ["installPhase"];
+
+    paths = [
+      fhsEnv
+      vuescan-desktop
+    ];
+    # installPhase = ''
+    # mkdir -p $out/bin
+    # mkdir -p $out/share/applications
+    # ln -s ${fhsEnv}/bin/vuescan $out/bin/vuescan
+    # cp ${vuescan-desktop}/share/applications/* $out/share/applications/
+    # '';
+
+    meta = with lib; {
+      description = "VueScan with GNOME Desktop integration";
+      platforms = platforms.linux;
+    };
+  }
