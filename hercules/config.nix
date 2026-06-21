cf4e5fffe745bacd7269891b8c64c3d4631ced5f
Jamie <jamie@quigley.xyz>
2026-06-21 15:56:01 +0100

    hercules: remove workarounds

--- hercules/config.nix
+++ hercules/config.nix
@@ -1,20 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   virtualisation.docker.enable = true;
   # services.vscode-server.enable = true;
-
-  systemd.services.lxc-activation-fix = {
-    description = "NixOS LXC Runtime Activation Fix";
-    after = ["systemd-tmpfiles-setup.service"];
-    before = ["sysinit.target"];
-    wantedBy = ["sysinit.target"];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "/nix/var/nix/profiles/system/activate";
-    };
-  };
-
-  systemd.tmpfiles.rules = [
-    "L+ /run/current-system - - - - /nix/var/nix/profiles/system"
-  ];
 }
